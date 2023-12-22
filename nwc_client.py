c6c03d14e322b2b5899196666a4ae7ccb913dfb3
Believethehype <tobias_baur@yahoo.com>
2023-12-22 23:33:14 +0100

    little cleanup

--- nwc_client.py
+++ nwc_client.py
@@ -54,16 +54,16 @@ def test_nwc():
 
     # PART 1 MAKE A NEW NWC Connection String
     if not os.getenv("TEST_NWC_STRING"):
+        # Note this doesn't have to be the client, it should be a users key, but in order to test we use our own key
         connectionstring = make_nwc_account(keys.public_key().to_hex(), "http://localhost:5001/api/new",
                                             lnbits_admin_key,
                                             lnbits_host, )
         print("NWC connection string: " + connectionstring)
 
+        # for now we store the single connection string in the .env, but this should be a database
         add_key_to_env_file("TEST_NWC_STRING", connectionstring)
     # PART 2 use the NWC String to zap
-    # Don't do this necesarily in one run, get the nwc string and add it here.
-    #  TODO Store the connection string in a db, manually add here if you already have one
-    # connectionstring = ""
+    # That's why we added ourself
     if os.getenv("TEST_NWC_STRING"):
         RELAY_LIST = ["wss://relay.damus.io", "wss://nostr-pub.wellorder.net", "wss://nos.lol", "wss://nostr.wine",
                       "wss://relay.nostfiles.dev", "wss://nostr.mom", "wss://nostr.oxtr.dev", "wss://relay.nostr.bg",
@@ -71,11 +71,11 @@ def test_nwc():
                       "wss://relay.snort.social", "wss://offchain.pub/",
                       "wss://nostr-pub.wellorder.net"]
 
-        # we zap npub1nxa4tywfz9nqp7z9zp7nr7d4nchhclsf58lcqt5y782rmf2hefjquaa6q8's profile 21 sats and say Cool stuff
+        # we zap npub1nxa4tywfz9nqp7z9zp7nr7d4nchhclsf58lcqt5y782rmf2hefjquaa6q8's profile 21 sats and say 'GFM''
         # Therefore we first do a regular zaprequest to obtain a bolt11 from their lud16
-        pubkey = PublicKey.from_bech32("npub1nxa4tywfz9nqp7z9zp7nr7d4nchhclsf58lcqt5y782rmf2hefjquaa6q8")
-        bolt11 = zaprequest("hype@bitcoinfixesthis.org", 21, "Cool Stuff", None,
-                            pubkey, keys, RELAY_LIST)
+        bolt11 = zaprequest("hype@bitcoinfixesthis.org", 21, "GFM", None,
+                            PublicKey.from_bech32("npub1nxa4tywfz9nqp7z9zp7nr7d4nchhclsf58lcqt5y782rmf2hefjquaa6q8"),
+                            keys, RELAY_LIST)
 
         # we then send a zap request via nwc using our nwc string, the bolt11 and our keys.
         nwc_zap(os.getenv("TEST_NWC_STRING"), bolt11, keys)
